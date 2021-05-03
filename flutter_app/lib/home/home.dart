7a88bb62f145c9767f2089ee4d80f40b44ccf0da
pvictor1206 <paulomagalhaes1206@gmail.com>
2021-05-03 13:08:55 -0300

    Aula 02

--- flutter_app/lib/home/home.dart
+++ flutter_app/lib/home/home.dart
@@ -10,14 +10,44 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //Barra
         title: Text("Calculadora de IMC"),
         centerTitle: true, // Titulo no centro
         backgroundColor: Colors.red, //Cor do fundo
         actions: [
-          IconButton(icon: Icon(Icons.refresh),
-              onPressed: () {},)
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
         ],
-      ), //Barra
+      ),
+      // Cor de fundo do App
+      backgroundColor: Colors.lightBlueAccent,
+      // Corpo
+      body: Column(
+        // Tudo que estiver na coluna vai ocupar toda a largura
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(Icons.person_pin_outlined, size: 120.0, color: Colors.red),
+          //Campo onde podera digitar texto
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.red)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.red, fontSize: 25.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.red)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.red, fontSize: 25.0),
+          )
+        ],
+      ),
     );
   }
 }
